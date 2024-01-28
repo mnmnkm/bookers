a87a70a5c3499ed01aeefb413ca70a2e155422ef
mnmnkm <152707508+mnmnkm@users.noreply.github.com>
2024-01-28 15:08:14 +0000

    [Add] フラッシュメッセージ(create/updateが成功した時)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,12 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -21,8 +25,12 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :new
+    end
   end
   
   def destroy
