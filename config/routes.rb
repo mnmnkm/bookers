1b17cc0ef6d98ee5732cc63e208f1448794aa251
mnmnkm <152707508+mnmnkm@users.noreply.github.com>
2024-01-28 06:32:17 +0000

    [Add] topページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   root to: 'homes#top'
-  resources :lists
+  resources :books
+  
+  post 'books' => 'books#create'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
